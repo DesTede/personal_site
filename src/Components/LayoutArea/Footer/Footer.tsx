9f75848cc19bb624b13e32e457842d2c5ab3ed4f
Des <desaleytede@gmail.com>
2024-09-20 20:14:51 +0300

    added images

--- src/Components/LayoutArea/Footer/Footer.tsx
+++ src/Components/LayoutArea/Footer/Footer.tsx
@@ -6,10 +6,10 @@ function Footer(): JSX.Element{
             <NavLink className={"aboutBtn"} to={"/aboutemporium"}>About Us</NavLink>
             &nbsp;|&nbsp;
             <a href={"https://github.com/DesTede"} target={"_blank"} rel="noreferrer">
-                <img src={"GitHub-Mark-ea2971cee799.png"} alt={"github link"}/>
+                <img src={"images/GitHub-Mark-ea2971cee799.png"} alt={"github link"}/>
             </a>
             <NavLink to={"mailto:desaleytede@gmail.com"}>
-                <img src={"/free-mail-1437261-1216865.webp"} alt={"mail link"}/>
+                <img src={"images/free-mail-1437261-1216865.webp"} alt={"mail link"}/>
             </NavLink>
             <a href={"https://www.facebook.com/"} target={"_blank"} rel="noreferrer">
                 <img src={"/46-facebook-512.webp"} alt={"facebook link"}/>

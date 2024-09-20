19efa64a9d62fcd2d322b8ea6eeaf6afc161304e
Des <desaleytede@gmail.com>
2024-09-20 19:51:04 +0300

    rel="noreferrer" update in Footer.tsx

--- src/Components/LayoutArea/Footer/Footer.tsx
+++ src/Components/LayoutArea/Footer/Footer.tsx
@@ -5,16 +5,16 @@ function Footer(): JSX.Element{
         <div className="Footer">
             <NavLink className={"aboutBtn"} to={"/aboutemporium"}>About Us</NavLink>
             &nbsp;|&nbsp;
-            <a href={"https://github.com/DesTede"} target={"_blank"}>
+            <a href={"https://github.com/DesTede"} target={"_blank"} rel="noreferrer">
                 <img src={"/GitHub-Mark-ea2971cee799.png"} alt={"github link"}/>
             </a>
             <NavLink to={"mailto:desaleytede@gmail.com"}>
                 <img src={"/free-mail-1437261-1216865.webp"} alt={"mail link"}/>
             </NavLink>
-            <a href={"https://www.facebook.com/"} target={"_blank"}>
+            <a href={"https://www.facebook.com/"} target={"_blank"} rel="noreferrer">
                 <img src={"/46-facebook-512.webp"} alt={"facebook link"}/>
             </a>
-            <a href={"https://www.instagram.com/"} target={"_blank"}>
+            <a href={"https://www.instagram.com/"} target={"_blank"} rel="noreferrer">
                 <img src={"/icon-media-social-instagram-free-vector.jpg"} alt={"instagram link"}/>
             </a>
         </div>

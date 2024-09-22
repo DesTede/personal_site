b45cc6336fd5365632cc03bbe7d7b49d31b253f2
Des <desaleytede@gmail.com>
2024-09-22 19:54:38 +0300

    About.tsx and Home.tsx updated

--- src/Sections/Home/Home.tsx
+++ src/Sections/Home/Home.tsx
@@ -1,13 +1,14 @@
+import "./Home.css"
+
+
 function Home(): JSX.Element {
     return (
         <div>
-         <h1>Desaley Tede</h1>
-            <h3>
-                Passionate Full stack developer
-            </h3>
+            
             <p>
-                
-                Full-Stack Developer specializing in Java & ReactJS, building dynamic, user-friendly applications.
+                <div className={"HomeHeader1"}>Desaley Tede</div>
+                <div className={"HomeHeader2"}>Passionate Full stack developer</div>
+                Full-Stack Developer specializing in Java, Spring & ReactJS, building dynamic, user-friendly applications.
             </p>
         </div>
     );

d0c0bd615da12e695536448c47c9a1260c1b0cff
Des <desaleytede@gmail.com>
2024-09-22 13:28:27 +0300

    @babel fixed

--- src/Components/LayoutArea/Layout/Layout.tsx
+++ src/Components/LayoutArea/Layout/Layout.tsx
@@ -1,6 +1,6 @@
 import {BrowserRouter} from "react-router-dom";
 import Navbar from "../../Navbar/Navbar";
-// import Footer from "../Footer/Footer";
+import Footer from "../Footer/Footer";
 
 function Layout(): JSX.Element{
     return (
